06ce9a855483ae7412ccdbe223bc2fa73e90a0bc
gparra3 <giseltpv@gmail.com>
2021-03-16 16:12:49 -0430

    Refactor Components

---
+++ src/components/Producto.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+
+
+const Productos = () => {
+    return (
+        <Fragment>
+
+            <h2 className="text-center my-5"> Listado de Productos </h2>
+
+            <table className="table table-striped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col"> Nombre </th>
+                        <th scope="col"> Precio </th>
+                        <th scope="col"> Acciones </th>
+                    </tr>
+        
+                </thead>
+            </table>
+
+            <tbody>
+
+            </tbody>
+        </Fragment>
+     );
+}
+ 
+export default Productos;
