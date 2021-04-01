30092615c25444cb98a7949d4bfa67919dfd0109
ginpg <you@example.com>
2021-03-31 22:14:32 -0400

    -

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 //Router es cada pagina
 import Header from './components/Header';
-import Productos from './components/Producto';
+import Productos from './components/Productos';
 import NuevoProducto from './components/NuevoProducto';
 import EditarProducto from './components/EditarProducto';
 
